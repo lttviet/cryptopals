cf879f9555c0d397b2fb8d0d5bfd6f50bf5c8253
lttviet <lttviet@gmail.com>
2017-04-22 22:31:01 -0400

    add challenge 1 and 2

---
+++ set1/challenge2/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		log.Fatal("Requires 2 hex strings.")
+	}
+
+	arr1, arr2 := decodeHexStr(os.Args[1]), decodeHexStr(os.Args[2])
+	arr := xor(arr1, arr2)
+
+	str := hex.EncodeToString(arr)
+	fmt.Println(str)
+}
+
+func xor(arr1, arr2 []byte) []byte {
+	var result []byte
+	for i := 0; i < len(arr1); i++ {
+		result = append(result, arr1[i]^arr2[i])
+	}
+
+	return result
+}
+
+func decodeHexStr(str string) []byte {
+	byteArr, err := hex.DecodeString(str)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return byteArr
+}
